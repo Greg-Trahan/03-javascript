2c88f6b17598e7341125af4ee344e3451e1a290c
Greg Trahan <trahan.greg@gmail.com>
2022-12-23 16:38:34 -0600

    Add password  exclusion criteria

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
-
 var generateBtn = document.querySelector("#generate");
 
 
@@ -41,29 +23,47 @@ function generatePassword(){
 
 //Ask user how many characters they want
 function numberOfCharacters(){
-  return parseInt(prompt("How many characters do you want your password to be?"));
+  var numCharacters = 0
+  while (numCharacters < 8 || numCharacters > 128){
+     numCharacters =  parseInt(prompt("How many characters do you want your password to be?"));
+     if (numCharacters >= 8 && numCharacters <= 128){
+      return numCharacters
+    }
+    else {
+      alert("Please enter a number between 8 and 128.")
+    }
+  }
 }
 
 //Ask user what types of characters they want and assign those characters
 function selectCharacterTypes(){
-  var characterTypes = []
   var finalOptions = ""
-  characterTypes.push(confirm("Do you want upper case characters?"));
-  characterTypes.push(confirm("Do you want lower case characters?"));
-  characterTypes.push(confirm("Do you want numbers?"));
-  characterTypes.push(confirm("Do you want special characters?"));
-  
-  if (characterTypes[0] === true){
-    finalOptions = finalOptions.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-  }
-  if (characterTypes[1] === true){
-    finalOptions = finalOptions.concat("abcdefghijklmnopqrstuvwxyz");
-  }
-  if (characterTypes[2] === true){
-    finalOptions = finalOptions.concat("1234567890");
-  }
-  if (characterTypes[3] === true){
-    finalOptions = finalOptions.concat("!@#$%^&*()_+=\?><~");
+
+  while (finalOptions === ""){
+    var characterTypes = []
+    
+    characterTypes.push(confirm("Do you want upper case characters?"));
+    characterTypes.push(confirm("Do you want lower case characters?"));
+    characterTypes.push(confirm("Do you want numbers?"));
+    characterTypes.push(confirm("Do you want special characters?"));
+
+    if (characterTypes[0] === true){
+      finalOptions = finalOptions.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+    console.log("hi")
+    if (characterTypes[1] === true){
+      finalOptions = finalOptions.concat("abcdefghijklmnopqrstuvwxyz");
+    }
+    if (characterTypes[2] === true){
+      finalOptions = finalOptions.concat("1234567890");
+    }
+    if (characterTypes[3] === true){
+      finalOptions = finalOptions.concat("!@#$%^&*()_+=\?><~");
+    }
+    if (finalOptions === ""){
+      alert("Please choose at least one character type");
+    }
+    console.log(characterTypes);
   }
   return finalOptions;
 }
